e9b6c42ae218a8154269af3ccfd8194fb32ee27f
skoolguy <greatguyajmal@gmail.com>
2016-02-29 06:56:59 +0530

    repaired the xor function

--- src/testPackage/bytePlay.java
+++ src/testPackage/bytePlay.java
@@ -33,6 +33,7 @@ public class bytePlay {
             
             //round--start
             rounded = keyxor(right,s);
+            
             s1 = s.substring(s.length()-1);
             s1+=s.substring(0,s.length()-1);
             s=s1;
@@ -42,9 +43,8 @@ public class bytePlay {
             temp = left;
             left = right;
             right = temp;
-            System.out.println("\tleft : "+left);
-            System.out.println("\tright : "+right);
-        }
+            
+        }   
         
         cipher = right+left;
         System.out.println("Final cipher = "+cipher);
@@ -57,11 +57,14 @@ public class bytePlay {
         String s1,s2;
         String text="";
         String temp,rounded;
-        
+        System.out.println("Key generation");
         for(int i=0;i<15;i++){
+            
             s1 = s.substring(s.length()-1);
             s1+=s.substring(0,s.length()-1);
             s=s1;
+            
+            System.out.println("After shift key : "+s);
         }
         
         System.out.println("deFiestel starting :");
@@ -76,6 +79,8 @@ public class bytePlay {
             s1 = s.substring(1);
             s1+=s.substring(0,1);
             s=s1;
+          
+            System.out.println("After shift key : "+s);
             //round--end
             
             
@@ -91,39 +96,8 @@ public class bytePlay {
         System.out.println("Final text = "+text);
         return text;
     }
-    
-   public String bin2string(String s){  //// gets a string with binary values and gives out the char thingy
-        String str = "";
-
-        for (int i = 0; i < s.length()/8; i++) {
-
-            int a = Integer.parseInt(s.substring(8*i,(i+1)*8),2);
-            str += (char)(a);
-        }
-        return str;
-   }
-    
-   public String string2bin(String s){
-       byte[] bytes = s.getBytes();
-         int bytesLength = bytes.length;
-         StringBuilder binary = new StringBuilder();
-         for (byte b : bytes)
-         {
-            int val = b;
-            for (int i = 0; i < 8; i++)
-            {
-               binary.append((val & 128) == 0 ? 0 : 1);
-               val <<= 1;
-            }
-            //binary.append(' ');
-         }
-         System.out.println("'" + s + "' to binary: " + binary);
-         
-         String res = binary.toString();
-         return res;
-   }
    
-   public String xor(String text, String key){         // XOR the text with the key [equal size]
+    public String xor(String text, String key){         // XOR the text with the key [equal size]
        System.out.println("EXECUTING XOR");
        String result="";
        byte[] textbyte = text.getBytes();
@@ -137,12 +111,12 @@ public class bytePlay {
            mval = mval^kval;
            
            result+=(char)mval;
-           
+           i++;
        }
        return result;
    }
        
-   public String keyxor(String text, String key){
+    public String keyxor(String text, String key){
        System.out.println("EXECUTING KEYXOR");
        System.out.println("\tKEY IS "+key);
        String result="";
--- src/testPackage/tests.java
+++ src/testPackage/tests.java
@@ -17,12 +17,43 @@ public class tests {
          
          String s1,s2;
          
-         //s1 = s.substring(1);
+         
          s1 = s.substring(1);
          s1+=s.substring(0,1);
-         //System.out.println("s1 = "+s1 );
+         
          System.out.println("s1 = "+s1);
     }
+    
+    public String bin2string(String s){  //// gets a string with binary values and gives out the char thingy
+        String str = "";
+
+        for (int i = 0; i < s.length()/8; i++) {
+
+            int a = Integer.parseInt(s.substring(8*i,(i+1)*8),2);
+            str += (char)(a);
+        }
+        return str;
+   }
+    
+   public String string2bin(String s){
+       byte[] bytes = s.getBytes();
+         int bytesLength = bytes.length;
+         StringBuilder binary = new StringBuilder();
+         for (byte b : bytes)
+         {
+            int val = b;
+            for (int i = 0; i < 8; i++)
+            {
+               binary.append((val & 128) == 0 ? 0 : 1);
+               val <<= 1;
+            }
+            //binary.append(' ');
+         }
+         System.out.println("'" + s + "' to binary: " + binary);
+         
+         String res = binary.toString();
+         return res;
+   }
     
     public static void main(String args[]){
         new tests();
